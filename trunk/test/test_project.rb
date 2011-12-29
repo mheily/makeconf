8b29b7ecb5e07c4a7e4f6ec1774dedbe43bbe3e0
mheily <mheily@e24ab224-07aa-4945-9ea2-6dfdaf20878c>
2011-12-29 03:25:08 +0000

    test project

--- trunk/test/test_project.rb
+++ trunk/test/test_project.rb
@@ -5,4 +5,19 @@ class ProjectTest < Test::Unit::TestCase
   def test_constructor
     assert_not_nil(Project.new)
   end
+
+  def test_attributes
+    Project.new do |p|
+      p.id = 'foo'
+      p.version = '1.0'
+      p.summary = 'A good program'
+      p.description = <<EOF
+        A really good program.
+        Trust me.
+EOF
+      p.author = 'John Smith'
+      p.license = 'BSD'
+      p.config_h = 'include/config.h'
+    end
+  end
 end
