21ee1fd5b9bf6f704a50ea1149880b3a1c2d51b4
Mark Heily <mheily@users.noreply.github.com>
2012-11-17 02:47:26 +0000

    rework simple test

--- testing/simple/configure.rb
+++ testing/simple/configure.rb
@@ -1,1 +1,44 @@
-../../configure.rb
+#!/usr/bin/env ruby
+#
+# Copyright (c) 2011 Mark Heily <mark@heily.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+$VERBOSE = true
+
+require 'makeconf'
+
+mc = Makeconf.new :minimum_version => 0.1
+
+mc.project = Project.new \
+    :id => 'simple', 
+    :version => 1.0
+
+mc.project.add Binary.new \
+    :id => 'hello', 
+    :sources => %w{main.c}
+
+mc.project.ac_check_header 'stdio.h'
+mc.project.ac_check_header 'does-not-exist.h'
+mc.project.ac_check_decl 'exit'
+mc.project.ac_check_decl 'symbol_that_does_not_exist'
+mc.project.ac_check_funcs 'epoll_create'
+mc.project.ac_check_funcs 'fgetln'
+mc.project.ac_check_funcs 'getline'
+
+mc.configure
+
+#check_symbol:
+#    fcntl.h: [ O_DOES_NOT_EXIST, O_RDWR ]
+#    pthread.h: [ pthread_exit ]
